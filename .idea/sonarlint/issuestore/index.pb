662b00b8e10658239d0c7f060f1dd0fdf7a44abe
dmeade3 <dcmstrat@gmail.com>
2017-09-14 18:57:27 +0100

    re

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,7 @@
+
+F
+src\GeneratorMain.java,7\5\75bf63a27f7047f3a7417f942f75eb479cb6f229
+I
+src\csvReader\Reader.java,3\a\3a182b001dcac287fe4a2bb4bf1ac187e78611af
+A
+csv\exercises.csv,6\1\61941b424b518be4b4e9c2d3a0bdf04be0eb9456
---
+++ src/csvReader/Reader.java
@@ -0,0 +1,25 @@
+package csvReader;
+
+public class Reader
+{
+    public static final String csvPath = System.getProperty("user.dir") + "\\csv";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    public static void main(String... args)
+    {
+        System.out.println(csvPath);
+    }
+}
