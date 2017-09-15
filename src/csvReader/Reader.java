ec1c91fc7a7ebad59f237bb96eb011abdeded63e
dmeade3 <dcmstrat@gmail.com>
2017-09-14 22:00:16 -0400

    re

--- src/csvReader/Reader.java
+++ src/csvReader/Reader.java
@@ -1,25 +1,62 @@
 package csvReader;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Reader
 {
-    public static final String csvPath = System.getProperty("user.dir") + "\\csv";
+    public static final String csvPath = System.getProperty("user.dir") + "\\csv\\";
 
+	public static List<String[]> readInFile(String filenameInCsvFolder)
+	{
+		String csvFile = csvPath + filenameInCsvFolder;
+		BufferedReader br = null;
+		String line = "";
+		String cvsSplitBy = ",";
 
+		List<String[]> returnList = new ArrayList<>();
 
+		try
+		{
+			br = new BufferedReader(new FileReader(csvFile));
+			while ((line = br.readLine()) != null) {
 
+				// use comma as separator
+				returnList.add(line.split(cvsSplitBy));
+			}
+		}
+		catch (IOException e)
+		{
+			e.printStackTrace();
+		}
+		finally
+		{
+			if (br != null)
+			{
+				try
+				{
+					br.close();
+				}
+				catch (IOException e)
+				{
+					e.printStackTrace();
+				}
+			}
+		}
 
+		return returnList;
+	}
 
-
-
-
-
-
-
-
-
-
-    public static void main(String... args)
-    {
-        System.out.println(csvPath);
-    }
+	public static void main(String[] args)
+	{
+		for (String[] line : readInFile("exercises.csv"))
+		{
+			System.out.println(Arrays.asList(line));
+		}
+	}
 }
